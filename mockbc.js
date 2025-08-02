8bbef3cb927c6c0538dc97eb6a5941e135a85f3d
Zundra Daniel <zundra.daniel@gmail.com>
2025-08-02 19:37:42 -0400

    update to use bc client/server seed

--- mockbc.js
+++ mockbc.js
@@ -5,7 +5,7 @@ class MockBC {
     this.intervalID = null;
     this.clientSeed = null;
     this.serverSeed = null;
-    this.nonce = 0;
+    this.nonce = 23228;
   }
 
   setResult(results) {
@@ -55,12 +55,20 @@ class MockBC {
     );
   }
 
+/* 
+Nonce 23228 (2.1x, 6.32x, 1.49x, 2.55x, 1.02x, 2.75x, 1.21x, 1.03x)
+Client Seed: Y83TpC2hj2SnjiNEDJwN
+Server Seed: 9b98254e8986dd95349fc754b4b087b5d0ddf9771026d68fe4ed661c869420b4
+Server Seed Hash: d7e30e322c0ec13033dd8b598a9261f7aa91b008ea9596c45a2f50f179b1a98a
+
+
+*/
   getClientSeed() {
-    return "abcdef8686341274e68915c96f01a4f3294a4155c4b1cdfaf14d86b84a976d55";
+    return "Y83TpC2hj2SnjiNEDJwN";
   }
 
   getServerSeed() {
-    return "zyxwvuts686341274e68915c96f01a4f3294a4155c4b1cdfaf14d86b84a976d55";
+    return "9b98254e8986dd95349fc754b4b087b5d0ddf9771026d68fe4ed661c869420b4";
   }
 
   getNonce() {
